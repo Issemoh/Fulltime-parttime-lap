bc63ee1f1c533e1ef704b6cba667cb95a7467310
ek5070pw <ek5070pw@T305009-1625273>
2020-01-30 13:20:25 -0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class IblStatus
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCredits = new System.Windows.Forms.TextBox();
+            this.btnWhatsStatus = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtCredits
+            // 
+            this.txtCredits.Location = new System.Drawing.Point(497, 64);
+            this.txtCredits.Name = "txtCredits";
+            this.txtCredits.Size = new System.Drawing.Size(100, 20);
+            this.txtCredits.TabIndex = 0;
+            // 
+            // btnWhatsStatus
+            // 
+            this.btnWhatsStatus.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnWhatsStatus.Location = new System.Drawing.Point(324, 144);
+            this.btnWhatsStatus.Name = "btnWhatsStatus";
+            this.btnWhatsStatus.Size = new System.Drawing.Size(256, 23);
+            this.btnWhatsStatus.TabIndex = 1;
+            this.btnWhatsStatus.Text = "What\'s my status?";
+            this.btnWhatsStatus.UseVisualStyleBackColor = false;
+            this.btnWhatsStatus.Click += new System.EventHandler(this.btnWhatsStatus_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblStatus.Location = new System.Drawing.Point(322, 226);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(258, 18);
+            this.lblStatus.TabIndex = 2;
+            this.lblStatus.Text = "Status Shown here";
+            this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblStatus.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(322, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "number of credit";
+            // 
+            // IblStatus
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnWhatsStatus);
+            this.Controls.Add(this.txtCredits);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.Name = "IblStatus";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCredits;
+        private System.Windows.Forms.Button btnWhatsStatus;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
